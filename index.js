95fc6a8a48867a07c7436f98785d61e6f9286537
ShahinHossaain <mdshahinhossaain@gmail.com>
2023-05-21 22:38:36 +0600

    delete done

--- index.js
+++ index.js
@@ -79,7 +79,13 @@ async function run() {
             res.send(result)
         })
 
-
+        app.delete('/toys/:id', async (req, res) => {
+            id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            console.log(query);
+            const result = await toysCollection.deleteOne(query);
+            res.send(result);
+        });
 
         app.patch('/toys/:id', async (req, res) => {
             const id = req.params.id;
